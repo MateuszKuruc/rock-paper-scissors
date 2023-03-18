0478ae886235cc8aa60536ab3bc5c16be4954eaa
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-03-18 22:41:15 +0100

    fix computer choice function and add new function
    fix computer choice function and add function for checking result of game

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 function getComputerChoice() {
-const choice = ['Rock', 'Paper', 'Scissors'];
-const computerChoice = choice[Math.floor(Math.random() * choice.length)];
-console.log(computerChoice);
+    const computerChoice = ['Rock', 'Paper', 'Scissors'];
+    return computerChoice[Math.floor(Math.random() * computerChoice.length)];
 }
 
-getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == 'rock' && computerSelection == 'Scissors') 
+    || (playerSelection == 'paper' && computerSelection == 'Rock') 
+    || (playerSelection == 'scissors' && computerSelection == 'Paper')) {
+        return `You have won! ${playerSelection} beats ${computerSelection}.`
+    }   else if ((playerSelection == 'rock' && computerSelection == 'Paper') 
+    || (playerSelection == 'paper' && computerSelection == 'Scissors') 
+    || (playerSelection == 'scissors' && computerSelection == 'Rock')) {
+        return `You have lost! ${computerSelection} beats ${playerSelection}.`
+    }   else {
+        return `It is a draw!`
+    }
+   
+    
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
+//let playerChoice = prompt('Choose Rock, Paper or Scissors');
+//let playerSelection = playerChoice.toLowerCase();
